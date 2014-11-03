129a7abea02707f6ebfef6e558af66bf9d64365a
AbelAlejandro <abel.alejandro@ymail.com>
2014-11-03 16:31:19 -0300

    fjyujyj

--- obj/Debug/models/cliente.js
+++
@@ -1,6 +0,0 @@
-﻿var mongoose = require('mongoose'), Schema = mongoose.Schema;
-var clienteSchema = new Schema({
-    nombre: String,
-    dni: Number
-});
-module.exports = mongoose.model('Cliente', clienteSchema);
